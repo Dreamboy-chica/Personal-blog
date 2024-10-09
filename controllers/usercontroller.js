918236c7cefb39ca435191d8cc941852efd29e47
Dreamboy chica <krishnamathura501@gmail.com>
2024-10-09 21:03:03 +0530

    v2

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,15 +1,28 @@
 let umodel = require("../models/usermodel");
 let bcrypt = require("bcrypt");
 
-let register = async (req, res) => {
-  try {
-    let hash = await bcrypt.hash(req.body.pwd, 10);
-    let data = new umodel({ ...req.body, pwd: hash });
-    await data.save();
-    res.json({ message: "Registration Done" });
-  } catch (err) {
-    res.json({ message: "Error in Registration" });
-  }
+let register = async (req, res) => 
+    {
+    try {
+
+        let chekobj = await umodel.findOne({ _uid: req.body._uid });
+
+        if (chekobj) 
+            {
+            res.json({ "message": "Email ID Already exists" });
+            } 
+        
+       else {
+            let hash = await bcrypt.hash(req.body.pwd, 10);
+            let data = new umodel({ ...req.body, pwd: hash });
+            await data.save();
+            res.json({ message: "Registration Done" });
+            }
+        } 
+    catch (err) {
+        console.error(err); 
+        res.json({ "message": "Error in Registration"});
+    }
 };
 
 module.exports = { register };
